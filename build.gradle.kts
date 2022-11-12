390b4b43afb040bb21ff773a1bbabdb0d048fbeb
Massimo Pavoni <maspavoni@gmail.com>
2022-11-12 17:55:09 +0100

    Single module change

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,21 @@
 plugins {
+    application
     id("org.sonarqube") version "3.5.0.2730"
+    id("org.springframework.boot") version "2.7.5"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:31.0.1-jre")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
