3e237b379dc00b22db1a54fe032f7b6fdfb1dd50
Massimo Pavoni <maspavoni@gmail.com>
2022-11-12 17:46:06 +0100

    Changes to dependencies

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,19 +1,18 @@
 plugins {
     application
     id("org.springframework.boot") version "2.7.5"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
-
-apply(plugin = "io.spring.dependency-management")
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     implementation("com.google.guava:guava:31.0.1-jre")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 application {
